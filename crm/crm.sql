517d29935717394b83598ee2a8828406201102d1
Patrick Stepien <patrick.stepien@kellogg.northwestern.edu>
2023-04-14 20:41:50 +0000

    practice

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,52 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS company;
+DROP TABLE IF EXISTS internal_contacts;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS cis;
+
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_id INTEGER
+  );
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+  );
+
+CREATE TABLE internal_contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+  );
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  internal_contact_id INTEGER,
+  contact_id INTEGER,
+  activity_type TEXT,
+  date_time TEXT,
+  notes TEXT
+  );
+
+  CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+  );
+
+  CREATE TABLE cis (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER
+  industry_id INTEGER
+  );
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,38 @@
 
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS follows;
+DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    real_name TEXT,
+    location TEXT
+);
+
+CREATE TABLE follows (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    followed_user_id INTEGER,
+    follower_user_id INTEGER
+);
+
+CREATE TABLE photos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    time_stamp TEXT
+);
+
+CREATE TABLE likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    photo_id INTEGER
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    photo_id INTEGER,
+    time_stamp TEXT
+);
